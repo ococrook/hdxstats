a142a9aed022ac569862ed494cbb2998ae499e9a
Oliver M. Crook <ococrook@gmail.com>
2021-07-21 11:08:53 +0100

    add some documentation and continue vignette

--- R/parse-function.R
+++ R/parse-function.R
@@ -1,4 +1,20 @@
-# parse deuterium data into QFeature objects
+##' Parse deuterium data into QFeature objects
+##' @param object An object of class DataFrame
+##' @param assayname The name of assay. The Default is incoperation.
+##' @param rownames The rownames of the features. Default is NULL, in which case
+##' the rownames are extract from the `sequence` and `charge` in the object
+##' @param filter Currently unused filter
+##' @param filterSCore Currently usused filter score
+##' @param sequence The name of the column where the peptide sequence is stored.
+##' Default is "pep_sequence"
+##' @param charge The name of the column where the peptide charge is stored.
+##' Default is "pep_chrage"
+##' @param  design The design which will become the column names
+##' @return An instance of class `QFeatures` storing the quantitative mass-spectrometry
+##' data
+##' @md
+##' 
+##' @rdname parse-function
 parseDeutData <- function(object,
                           assayname = "incoperation",
                           rownames = NULL,
@@ -7,8 +23,7 @@ parseDeutData <- function(object,
                           filterScore = 0.9,
                           sequence = "pep_sequence",
                           charge = "pep_charge",
-                          design = NULL,
-                          ecol = NULL){
+                          design = NULL){
     
     stopifnot("object must be of class DataFrame"= class(object) == "DFrame")
     stopifnot("You must provide a design"= is.null(design) == FALSE)
