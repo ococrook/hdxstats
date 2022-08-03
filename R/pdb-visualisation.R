a4b52dd8ebdc1d7a224d793a99bc98fdc93b3b5c
Broncio Aguilar Sanjuan <sanjuan@dragon000.stats.ox.ac.uk>
2022-08-03 18:55:15 +0100

    added module with functions used for HDX-MS data colour encoding

---
+++ R/pdb-visualisation.R
@@ -0,0 +1,93 @@
+##' Color function
+##' @author Broncio Aguilar-Sanjuan
+##' @param dataset The dataset for which numerical values will be colour enconded
+##' @param scale_limits You can force a range of numerical values to be mapped only
+##' @return Returns a color function
+##' @md
+##' 
+##' @rdname pdb-visualisation
+define_color_function <- function(dataset, 
+                                  scale_limits = NULL){
+  
+    colormap <- colorRamp(brewer.pal(n=9, name="RdBu"))
+    if (is.null(scale_limits)) {
+      # By default work out the numerical domain from the data
+      scale_limits <- c(min(dataset[!is.na(dataset)]), 
+                        max(dataset[!is.na(dataset)]))
+      
+      print("Negative values will be coloured in Blue, and positive ones on Red")
+    }
+    
+    print(paste("Your scale limits are ", round(scale_limits, 2)))
+    
+    colormap <- colorRamp(brewer.pal(8, "Blues"))
+    domain <- c(0.0, abs(scale_limits[1]))
+    color_function_deprotected <- col_bin(colormap, domain, na.color="#808080")
+    
+    colormap <- colorRamp(brewer.pal(8, "Reds"))
+    domain <- c(0.0, abs(scale_limits[2]))
+    color_function_protected <- col_bin(colormap, domain, na.color="#808080")
+    
+    output_function <- function(x){
+      if (!is.na(x) && x >= 0.0){
+        return(color_function_protected(abs(x)))
+      }else{
+        return(color_function_deprotected(abs(x)))
+      }
+    }
+  
+    return(output_function)
+}
+
+##' Map estimated (de)protection values to Blue-White-Red colormap values
+##' @author Broncio Aguilar-Sanjuan
+##' @param dataset A list of estimated protection (positive) and deprotection (negative) values per residue
+##' @param pdb_filepath The path to the PDB file for mapping of (de)protection values
+##' @param scale_limits You can force a range of numerical values to be mapped only
+##' @return Returns a list of colours and residue numbers to be inputted into NGLVieweR, 
+##' with protection values in Red and deprotection values in Blue, and NA values in Grey.
+##' @md
+##' 
+##' @rdname pdb-visualisation
+hdx_to_pdb_colours <- function(dataset,
+                               pdb_filepath,
+                               scale_limits = NULL){
+  
+    # Extract residue numbers from available residues in PDB coordinates ---
+    
+    pdb_content <- read.pdb(pdb_filepath)
+    sequence_from_pdb <- pdbseq(pdb_content)
+    sequence_residue_numbers_pdb <- strtoi(row.names(data.frame(sequence_from_pdb)))
+    
+    # Report available data ---
+    
+    n_residues_from_pdb <- nchar(paste(sequence_from_pdb, collapse=""))
+    print(paste("Your HDX input dataset has", length(colnames(dataset)), "entries"))
+    print(paste("And excluding NA data you only have", length(dataset[!is.na(dataset)]), "entries"))
+    print(paste("However, your input PDB has only", n_residues_from_pdb, "residues in total"))
+    
+    # Work out residues and (de)protection values that can be mapped onto PDB ---
+    # Note: some residues are likely to be missing in the PDB
+    
+    sequence_residue_numbers_hdx <- seq(length(dataset)) # NEED TO TWEAK THIS!!!
+    residue_numbers_hdx_pdb <- intersect(sequence_residue_numbers_hdx, sequence_residue_numbers_pdb)
+    
+    dataset_for_pdb_mapping <- dataset[residue_numbers_hdx_pdb]
+    
+    aa_sequence_from_pdb <- sequence_from_pdb[sequence_residue_numbers_pdb %in% residue_numbers_hdx_pdb]
+    aa_sequence_from_pdb <- as.vector(aa_sequence_from_pdb)
+    
+    pdb_viewer_data <- list("values"= dataset_for_pdb_mapping,
+                            "residues"= residue_numbers_hdx_pdb,
+                            "aa"= aa_sequence_from_pdb)
+    
+    # Define colormap according to scale_limits--
+    
+    color_function <- define_color_function(dataset, scale_limits)
+    
+    residue_selections <- pdb_viewer_data$residues
+    df <- data.frame(x=unlist(lapply(pdb_viewer_data$values, color_function)), y=residue_selections)
+    color_parameters <- to_list(for(i in 1:length(residue_selections)) c(df$x[i], df$y[i]))
+
+    return(color_parameters)
+}
