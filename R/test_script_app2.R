8918eec6d2cdac735aa6e8dae478c3fe0dd098f8
Broncio Aguilar Sanjuan <sanjuan@dragon000.stats.ox.ac.uk>
2022-08-18 17:45:54 +0100

    added new function for analysis and test cases

--- R/test_script_app2.R
+++ R/test_script_app2.R
@@ -56,8 +56,6 @@ preprocess_data <- function(data,
                             design = new_object.colnames,
                             quantcol = initial_column:last_column)
   
-
-  
   # Normalise data 
   if (normalise) {
     print("INFO: Normalised data")
@@ -91,21 +89,21 @@ preprocess_data <- function(data,
 ##' 
 extract_hdx_data <- function(data_path) {
 
-  if (file_test("-f", data_path)) {
+  if (file_test("-f", data_path)){
     if (file_ext(data_path) == "csv") {
       print("You gave me a CSV file for your HDX-MSM data")
       
       data <- read_csv(data_path)
       data.type <- "csv"
       }
-    else if (file_ext(data_path) == "rsd") {
+    else if (file_ext(data_path) == "rsd"){
       print("You gave me a RSD file for your HDX-MSM data")
       print("I will assume your input data has alreayd been pre-processed")
       
       data <- readRDS(data_path)
       return(data)
     }
-    else {
+    else{
       print("You provided an input file format that I cannot recognise")
       print("Provide a valid input. I will not continue execution.")
       return(NULL)
@@ -117,7 +115,7 @@ extract_hdx_data <- function(data_path) {
   }
     
   # 2. Pre-process data
-  if (data.type == "csv") {
+  if (data.type == "csv"){
     print("INFO: I will pre-process your data parse it using QFeatures ...")
     data <- preprocess_data(data, normalise = FALSE, save = FALSE)
     print("INFO: I pre-processed you input CSV data content and now it's available as a QFeatures instance")
@@ -127,34 +125,118 @@ extract_hdx_data <- function(data_path) {
   
 }
 
-##' Visualise data
+##' Apply functional analysis to an input QFeatures data object
+##' @author Broncio Aguilar-Sanjuan and Oliver Crook
+##' @param data Input QFeatures data object or a selection 
+##' @param peptide_selection Peptide string chain, e.g., 'DKELKAKGKSAL_4'
+##' @param method Options: 
+##'               "fit"  for a regular fitting
+##'               "dfit" for differential fitting
+##' @param starting_parameters To optimise for kinetic model based on 'formula'
+##' @param formula Functional form or the fitting curve to optimise its parameters given 'starting_parameters'
+##'                e.g., 'formula = value ~ a * (1 - exp(-0.07*(timepoint))))'
+##' @return Results from Empirical Bayes on fitted kinetic functionals or curves of Deuterium uptake
+##' @md
+##'  
+analyse_kinetics <- function(data, 
+                             peptide_selection = NULL,
+                             method = NULL,
+                             starting_parameters = list(a = NULL, b = 0.001,  d = NULL, p = 1),
+                             formula = NULL){
+  
+  if (method == "fit"){
+    fitting_method <- fitUptakeKinetics
+    fitted_models <- fitting_method(object = data,
+                                    feature = peptide_selection,
+                                    start = starting_parameters)
+    functional_analysis <- processFunctional(object = data,
+                                             params = fitted_models)
+    
+    results <- list("fitted_models" = fitted_models, "functional_analysis" = functional_analysis)
+    return(results)
+  }
 
-
-##' Apply functional analysis
-analyse_data <- function(a, b){
-  return()
+  else if (method == "dfit"){
+    fitting_method <- differentialUptakeKinetics
+    
+    if (is.null(formula)){
+      
+      results <- fitting_method(object = data,
+                                feature = peptide_selection,
+                                start = starting_parameters)
+    }
+    
+    else{
+      results <- fitting_method(object = data,
+                                feature = peptide_selection,
+                                start = starting_parameters,
+                                formula = formula)
+    }
+    return(results)
+  }
 }
 
-##'  
-
-
-# 1. Extract the data, and pre-process if necessary
 #data_path <- "inst/extdata/Project_2_SecA_Cluster_Data.csv"
-data_path <- "/homes/sanjuan/R/x86_64-pc-linux-gnu-library/4.1/hdxstats/extdata/MBP.csv"
+#data_path <- "/homes/sanjuan/R/x86_64-pc-linux-gnu-library/4.1/hdxstats/extdata/MBP.csv"
 
-data_filepath <- "vignettes/data/MBPqDF.rsd"
-hdx_data <- extract_hdx_data(data_filepath)
-results <- analyse_hdx_data(hdx_data)
-graphics <- visualise_hdx_data(results, type="kinetics")
+
+data_filepath <- "vignettes/data/MBPqDF.rsd" # or, CSV file
+hdx_data <- extract_hdx_data(data_filepath) # DONE
+
+
+# TEST 1
+# INPUT
+data_selection <- hdx_data[,1:24]
+all_peptides <- rownames(data_selection)[[1]]
+starting_parameters <- list(a = NULL, b = 0.001,  d = NULL, p = 1)
+# OUTPUT
+results <- analyse_kinetics(data = data_selection, 
+                            method = "fit", 
+                            peptide_selection = all_peptides, 
+                            start = starting_parameters)
+# TEST 2
+# INPUT
+data_selection <- hdx_data[,61:100]
+all_peptides <- rownames(data_selection)[[1]]
+starting_parameters <- list(a = NULL, b = 0.001,  d = NULL, p = 1)
+# OUTPUT
+results <- analyse_kinetics(data = data_selection, 
+                            method = "fit", 
+                            peptide_selection = all_peptides, 
+                            start = starting_parameters)
+
+# TEST 3
+# INPUT
+data_selection <- hdx_data[,1:100]
+all_peptides <- rownames(data_selection)[[1]] # get all peptides
+starting_parameters <- list(a = NULL, b = 0.0001,  d = NULL, p = 1)
+# OUTPUT
+results <- analyse_kinetics(data = data_selection, 
+                            method = "dfit", 
+                            peptide_selection = all_peptides[37], 
+                            start = starting_parameters)
+
+# TEST 4
+# HOIP BINDING <<<<< ----- NEXT!!
+
+graphics <- visualise_hdx_data(results, type="kinetics") # <<<<--- NEXT
+graphics <- visualise_hdx_data(results, type="forest")
 graphics <- visualise_hdx_data(results, type="manhatten")
 graphics <- visualise_hdx_data(results, type="epitope", level="peptide")
-graphics <- visualise_hdx_data(results, type="epitope", level="residue")
-graphics <- visualise_hdx_data(results, type="protection", level="peptide")
-graphics <- visualise_hdx_data(results, type="protection", level="residue")
+graphics <- visualise_hdx_data(results, type="epitope", level="residue") # Return heatmap
+graphics <- visualise_hdx_data(results, type="protection", level="peptide") # Return heatmap
+graphics <- visualise_hdx_data(results, type="protection", level="residue") # Return heatmap
 graphics <- visualise_hdx_data(results, type="protection", level="residue", pdb="my_pdb_path")
 
-# 2. Visualise the data
+# GUI: Make GUI by assembling these building blocks
 
-# Create heatmaps to visualise content and a few data points
 
-# 3. Analyse data (statistic modelling)
+
+
+
+# This condenses Tutorial 1
+# Note that differentialUptake can only be applied to all data in relation to a single selected peptide
+# If feature = all_peptides, this will procude a NULL output
+# THi ALREADY PROCESS functionals by default
+#all_peptides <- rownames(data_qDF)[[1]] # get all peptides
+#peptide_selection <- all_peptides[37]
