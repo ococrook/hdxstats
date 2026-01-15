20d69ce4912293bee26a74598386cb7f6c1c5e05
Oliver M. Crook <ococrook@gmail.com>
2026-01-15 12:39:10 +0000

    Update hdxstat-class.R

--- R/hdxstat-class.R
+++ R/hdxstat-class.R
@@ -32,7 +32,7 @@ setOldClass("gg")
 .hdxstatmodel <- setClass("HdxStatModel",
                           slots = c(nullmodel = "nls",
                                     alternative = "nlsList",
-                                    vis = "gg",
+                                    vis = "ggplot",
                                     method = "character",
                                     formula = "formula"),
                           validity = function(object){
